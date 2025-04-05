ab469aa8ca726d4179547f412ae406c3d54745cb
dishant dudhat <dobariyadishant@gmail.com>
2025-04-05 14:21:23 +0530

    Added state-lock file

--- conf/terraform.tf
+++ conf/terraform.tf
@@ -5,4 +5,10 @@ terraform {
         version = "5.91.0"
     }
   }
+  backend "s3" {
+    bucket = "terraform-state"
+    key = "terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "lock-state-dynamodb"
+  }
 }
